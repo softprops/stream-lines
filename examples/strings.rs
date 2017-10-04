0955d3ec8e4cf251b9c4bc9ffefd1f964f5e6ca0
softprops <d.tangren@gmail.com>
2017-10-04 19:41:48 -0400

    generalize over types of results

---
+++ examples/strings.rs
@@ -0,0 +1,17 @@
+extern crate futures;
+extern crate stream_lines;
+extern crate tokio_core;
+
+use std::string::FromUtf8Error;
+
+use futures::Stream;
+use futures::stream::iter_ok;
+use tokio_core::reactor::Core;
+
+fn main() {
+    let mut core = Core::new().unwrap();
+    let chunks = vec!["hello ", "world\n", "\n", "what a\nlovely", "\nday\n"];
+    let stream = iter_ok::<_, FromUtf8Error>(chunks);
+    let print = stream_lines::strings(stream).for_each(|line| Ok(println!("{}", line)));
+    core.run(print).unwrap();
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,37 +1,59 @@
+//! Lined-oriented Rustlang [Streams](http://alexcrichton.com/futures-rs/futures/stream/index.html)
+//!
+//! Streams represent spools of results computed asyncronously. They are the async
+//! analog to Rustlang's Iterator type
+//!
+//! This crate represents a Stream transformer over Stream's of `AsRef<[u8]>`
+//! that result in Stream's of line-oriented results.
+//!
+
+#![warn(missing_docs)]
+
 extern crate futures;
 
 use std::mem::replace;
 use std::string::FromUtf8Error;
 
 use futures::{Async, Poll, Stream};
-use futures::stream::{Fuse, iter_ok};
+use futures::stream::Fuse;
 
-struct Lines<S: Stream> {
+const NEWLINE: u8 = b'\n';
+
+/// Converts to a fused stream into a line oriented stream
+pub struct Lines<S: Stream, O, E> {
     buffered: Option<Vec<u8>>,
     stream: Fuse<S>,
+    into: fn(Vec<u8>) -> Result<O, E>,
 }
 
-impl<S: Stream> Lines<S> {
-    pub fn new(stream: S) -> Lines<S> {
+/// A lined oriented stream of `Strings`
+pub fn strings<S>(s: S) -> Lines<S, String, FromUtf8Error>
+where
+    S: Stream,
+{
+    Lines::new(s, String::from_utf8)
+}
+
+impl<S: Stream, O, E> Lines<S, O, E> {
+    /// Creates a new `Lines` instance that wraps another stream
+    pub fn new(stream: S, into: fn(Vec<u8>) -> Result<O, E>) -> Self {
         Lines {
             buffered: None,
             stream: stream.fuse(),
+            into: into,
         }
     }
 
-    fn process(
-        &mut self,
-        flush: bool,
-    ) -> Option<Result<String, FromUtf8Error>> {
+    fn next(&mut self, flush: bool) -> Option<Result<O, E>> {
         let buffered = replace(&mut self.buffered, None);
         if let Some(ref buffer) = buffered {
-            let mut split = buffer.splitn(2, |c| *c == b'\n');
+            let mut split = buffer.splitn(2, |c| *c == NEWLINE);
             if let Some(first) = split.next() {
                 if let Some(second) = split.next() {
                     replace(&mut self.buffered, Some(second.to_vec()));
-                    return Some(String::from_utf8(first.to_vec()));
+                    return Some((self.into)(first.to_vec()));
                 } else if flush {
-                    return Some(String::from_utf8(first.to_vec()));
+                    return Some((self.into)(first.to_vec()));
                 }
             }
         }
@@ -40,19 +62,19 @@ impl<S: Stream> Lines<S> {
     }
 }
 
-impl<S> Stream for Lines<S>
+impl<S, O, E> Stream for Lines<S, O, E>
 where
     S: Stream,
     S::Item: AsRef<[u8]>,
-    S::Error: From<FromUtf8Error>,
+    S::Error: From<E>,
 {
-    type Item = String;
+    type Item = O;
     type Error = S::Error;
-    fn poll(&mut self) -> Poll<Option<String>, S::Error> {
+    fn poll(&mut self) -> Poll<Option<O>, S::Error> {
         match self.stream.poll()? {
             Async::NotReady => Ok(Async::NotReady),
             Async::Ready(None) => {
-                match self.process(true) {
+                match self.next(true) {
                     Some(Ok(line)) => Ok(Async::Ready(Some(line))),
                     Some(Err(err)) => Err(err.into()),
                     None => Ok(Async::Ready(None)),
@@ -64,7 +86,7 @@ where
                 } else {
                     self.buffered = Some(chunk.as_ref().to_vec());
                 }
-                match self.process(false) {
+                match self.next(false) {
                     Some(Ok(line)) => Ok(Async::Ready(Some(line))),
                     Some(Err(err)) => Err(err.into()),
                     None => Ok(Async::NotReady),
@@ -77,12 +99,13 @@ where
 #[cfg(test)]
 mod tests {
     use super::*;
+    use futures::stream::iter_ok;
     #[test]
     fn it_works() {
         let chunks =
             vec!["hello ", "world\n", "\n", "what a\nlovely", "\nday\n"];
         let stream = iter_ok::<_, FromUtf8Error>(chunks);
-        let mut lines = Lines::new(stream);
+        let mut lines = strings(stream);
         assert_eq!(lines.poll().unwrap(), Async::NotReady);
         assert_eq!(
             lines.poll().unwrap(),
